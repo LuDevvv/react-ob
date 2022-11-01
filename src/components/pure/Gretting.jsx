c7f219e339cfdc1b10ff12571f15d5c83ccf0896
LuDev <ludevgarcialouis@gmail.com>
2022-11-01 00:25:40 -0400

    Initial commit

---
+++ src/components/pure/Gretting.jsx
@@ -0,0 +1,14 @@
+// Class component
+
+import { Component } from "react";
+
+export class Gretting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello World</div>;
+  }
+}
